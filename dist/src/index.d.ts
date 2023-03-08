bca2130d8d8aa31af5e5d03f3492d303fe8f7d0c
Shamarin Alexandr <alexsandrshamarin@yandex.ru>
2023-03-09 00:25:04 +0300

    Build

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,6 +1,8 @@
 import { VirtualTable, VirtualTableProps } from './table';
-import { Grid, IGridProps, IGridState } from './grid';
+import { Grid, IGridProps, IGridState, OnScrollProps, OnScrollCallback } from './grid';
 import { ColumnType, ColumnsType, ScrollViewSize, ScrollConfig } from './interfaces';
+export type GridOnScrollProps = OnScrollProps;
+export type GridOnScrollCallback = OnScrollCallback;
 export type { VirtualTableProps, IGridProps, IGridState, ColumnType, ColumnsType, ScrollViewSize, ScrollConfig };
 export { VirtualTable, Grid };
 export default VirtualTable;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { VirtualTable, VirtualTableProps } from './table'
-import { Grid, IGridProps, IGridState, OnScrollCallback } from './grid';
+import { Grid, IGridProps, IGridState, OnScrollProps, OnScrollCallback } from './grid';
 import { ColumnType, ColumnsType, ScrollViewSize, ScrollConfig } from './interfaces';
+
+export type GridOnScrollProps = OnScrollProps;
+export type GridOnScrollCallback = OnScrollCallback;
 
 export type { VirtualTableProps, IGridProps, IGridState, ColumnType, ColumnsType, ScrollViewSize, ScrollConfig }
 export { VirtualTable, Grid };
