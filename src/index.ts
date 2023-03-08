d61ef02ab4b1565f11349a0b0848c391975059c8
Shamarin Alexandr <alexsandrshamarin@yandex.ru>
2023-03-09 00:21:20 +0300

    Build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { VirtualTable, VirtualTableProps } from './table'
-import { Grid, IGridProps, IGridState } from './grid';
+import { Grid, IGridProps, IGridState, OnScrollCallback } from './grid';
 import { ColumnType, ColumnsType, ScrollViewSize, ScrollConfig } from './interfaces';
 
 export type { VirtualTableProps, IGridProps, IGridState, ColumnType, ColumnsType, ScrollViewSize, ScrollConfig }
